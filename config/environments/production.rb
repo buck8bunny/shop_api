18e1094850a876d94e4764d5b9c8e2fb08ba354a
buck8bunny <monstera211@gmail.com>
2025-01-03 23:55:44 +0200

    modified:   config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,6 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.hosts << "https://shop-s2wc.onrender.com"  # Добавьте ваше доменное имя Render
+
 end
