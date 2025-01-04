e746c8f975bd8fda0dbb5b9064f448787a888473
buck8bunny <monstera211@gmail.com>
2025-01-05 00:18:29 +0200

    modified:   config/initializers/cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
       credentials: true,
       headers: :any,
-      credentials: true,
-      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client', 'Access-Token', 'Authorization', 'Client', 'Uid'],
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 
   end
