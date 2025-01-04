6adf522c3f7767fc184acb7ce95744557068e49e
buck8bunny <monstera211@gmail.com>
2025-01-04 20:57:54 +0200

    config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.hosts << "shop-api-308l.onrender.com"
+  config.hosts << "https://shop-api-308l.onrender.com"
   # config/environments/production.rb
   config.public_file_server.enabled = true
   config.public_file_server.index_name = 'index.html'  # Для фронтенда
