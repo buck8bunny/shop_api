634dfe2fcefd58aa319f0624e464191dfa30ff65
buck8bunny <monstera211@gmail.com>
2025-01-04 21:06:41 +0200

    modified:   config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,8 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.hosts << "https://shop-api-308l.onrender.com"
+  config.hosts << "shop-api-308l.onrender.com"
+  config.hosts << "shop-api-indol-nu.vercel.app"
   # config/environments/production.rb
   config.public_file_server.enabled = true
   config.public_file_server.index_name = 'index.html'  # Для фронтенда
