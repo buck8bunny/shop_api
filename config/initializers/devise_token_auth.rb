c9c1cf3d3295e867d0009828e1c288c10726480e
buck8bunny <98045621+buck8bunny@users.noreply.github.com>
2025-01-21 12:38:49 +0200

    Update devise_token_auth.rb

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -1,6 +1,6 @@
 DeviseTokenAuth.setup do |config|
 
-  config.change_headers_on_each_request = true
+  config.change_headers_on_each_request = false
   config.token_lifespan = 2.weeks
 
   config.token_cost = Rails.env.test? ? 4 : 10
